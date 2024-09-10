8a9215473142abd4b9e938f146664fd2b75ef261
Sanyog <sanyog1719210220@gmail.com>
2024-09-10 16:48:36 +0530

    How npm, and parcel works, how it helps React to be fast

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const headings = React.createElement("h1", {}, "Hello world!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(headings);
